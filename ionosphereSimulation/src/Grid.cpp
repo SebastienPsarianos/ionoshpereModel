ed469862d7fdbee847dbc08ed479546931166533
Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro-2.local>
2025-12-01 16:20:31 -0500

    updating stuff

--- ionosphereSimulation/src/Grid.cpp
+++ ionosphereSimulation/src/Grid.cpp
@@ -1,4 +1,7 @@
 #include "Grid.hpp"
+
+#include <iomanip>
+#include <iostream>
 #include <stdexcept>
 
 Grid::Grid(size_t nTh, size_t nPh, double initialValue)
@@ -9,6 +12,20 @@ double& Grid::operator()(size_t th, size_t ph) {
         throw std::out_of_range("Index out of range");
     }
     return _grid[th * nPh + ph];
+}
+
+// TODO: Redo this properly.
+std::ostream& Grid::printWithCoords(std::ostream& out, GridSet<Ang>& coords) {
+    out << std::scientific << std::setprecision(6);
+
+    for (size_t th = 0; th < nTh; th++) {
+        for (size_t ph = 0; ph < nPh; ph++) {
+            out << coords(Ang::TH, th, ph) << " " << coords(Ang::PH, th, ph)
+                << " " << (*this)(th, ph) << "\n";
+        }
+    }
+    out << std::endl;
+    return out;
 }
 
 template <ValidEnum T>
@@ -43,6 +60,29 @@ double& GridSet<T>::operator()(T grid, unsigned int th, unsigned int ph) {
         throw std::out_of_range("Index PHI out of range");
     }
     return _grids.at(grid)(th, ph);
+}
+
+template <ValidEnum T>
+std::ostream& GridSet<T>::printWithCoords(std::ostream& out,
+                                          GridSet<Ang>& coords) {
+    out << std::scientific << std::setprecision(6);
+
+    for (size_t th = 0; th < nTh; th++) {
+        for (size_t ph = 0; ph < nPh; ph++) {
+            out << coords(Ang::TH, th, ph) << " " << coords(Ang::PH, th, ph)
+                << " ";
+
+            // Print out values for all of the grid values in a row
+            for (int grid = 0; grid < static_cast<int>(T::COUNT); grid++) {
+                out << this->_grids.at(static_cast<T>(grid))(th, ph) << " ";
+            }
+
+            out << "\n";
+        }
+    }
+
+    out << std::endl;
+    return out;
 }
 
 template class GridSet<Sigma>;
