d6b649f415a2b707ca60491e9aa52cb2ac88ad81
sebastienpsarianos <sebastien@psarianos.com>
2026-01-30 14:39:12 -0500

    fixing boundary condition and orchestrator

--- orchestration/orchestrator.py
+++ orchestration/orchestrator.py
@@ -5,19 +5,23 @@ import glob
 
 LEGACY_LOCATION = "../legacySolution/src/"
 LEGACY_EXEC = "ionosphere.exe"
-DATA_FOLDER = "../ionosphereSimulation/data/"
+NEW_DATA_FOLDER = "../ionosphereSimulation/data/"
 NEW_BUILD_LOCATION = "../ionosphereSimulation/build/"
+PLOTTING_FOLDER = "../data"
 NEW_EXEC = "IonosphereSolver"
 
-FILES = ["sim_input.txt",
-         "sig_out.txt",
-         "dsig_out.txt",
-         "pot_out.txt",
-         "e_out.txt"]
+
+LEGACY_OUTPUT = ["sim_input.txt",
+                 "sig_out.txt",
+                 "dsig_out.txt",
+                 "pot_out.txt",
+                 "e_out.txt"]
+NEW_OUTPUT = ["conductance.txt", "dconductance.txt",
+              "kappa.txt", "solvedEField.txt", "solvedPotential.txt"]
 
 
 def cleanup():
-    txt_files = glob.glob(os.path.join(DATA_FOLDER, '*.txt'))
+    txt_files = glob.glob(os.path.join(NEW_DATA_FOLDER, '*.txt'))
 
     for file_path in txt_files:
         try:
@@ -46,10 +50,10 @@ def runProcesses():
 
     print("Ran legacy simulation")
 
-    for FILE in FILES:
+    for FILE in LEGACY_OUTPUT:
         if os.path.exists(LEGACY_LOCATION + FILE):
             print("Found " + FILE + " and moved to" +
-                  shutil.move(LEGACY_LOCATION + FILE, DATA_FOLDER + FILE))
+                  shutil.move(LEGACY_LOCATION + FILE, NEW_DATA_FOLDER + FILE))
         else:
             print("Failed to find " + FILE)
 
@@ -65,13 +69,23 @@ def runProcesses():
 
     print("New simulation build successful")
 
-    if not os.path.exists(LEGACY_LOCATION + LEGACY_EXEC):
+    if not os.path.exists(os.path.join(NEW_BUILD_LOCATION, NEW_EXEC)):
         print("Compiled file not found")
         return 0
 
     subprocess.run(["./" + NEW_EXEC, "../data/sim_input.txt"],
                    cwd=NEW_BUILD_LOCATION,
                    check=True)
+
+    if not os.path.exists(PLOTTING_FOLDER):
+        os.makedirs(PLOTTING_FOLDER)
+
+    if os.path.exists(NEW_DATA_FOLDER):
+        files = glob.glob(NEW_DATA_FOLDER + "*")
+        for source in files:
+            filename = os.path.basename(source)
+            print("Found " + source + " and moved to" +
+                  shutil.move(source, os.path.join(PLOTTING_FOLDER, filename)))
 
 
 if __name__ == "__main__":
